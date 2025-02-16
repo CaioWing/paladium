0aed1f839e429a415fd95286edb5c1dceeb281d9
Caio Wingeter <castilhocaio1164@gmail.com>
2025-02-15 21:36:24 -0300

    Add preprocessing utility for license plate images

---
+++ utils/preprocessing.py
@@ -0,0 +1,8 @@
+import cv2
+
+
+def preprocess_license_plate(image):
+    """Melhora o contraste para OCR."""
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # _, thresh = cv2.threshold(gray, 64, 255, cv2.THRESH_BINARY_INV)
+    return image
