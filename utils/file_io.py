ce0595ad76c16efaf14f2ff5d5f032c785572f53
Caio Wingeter <castilhocaio1164@gmail.com>
2025-02-17 20:38:39 -0300

    Refactor CSV writing logic in file_io.py for improved clarity and accuracy in vehicle data output

--- utils/file_io.py
+++ utils/file_io.py
@@ -5,8 +5,9 @@ from utils.vehicle_data import Vehicle
 
 def write_csv(results, output_path):
     """
-    Escreve os resultados em um arquivo CSV com o seguinte cabeçalho:
+    Escreve os resultados finais em um arquivo CSV com o cabeçalho:
       frame_nmr, car_id, car_bbox, license_plate_bbox, license_plate_bbox_score, license_number, license_number_score
+    Cada linha representa um veículo único.
     """
     with open(output_path, "w", newline="") as f:
         writer = csv.writer(f)
@@ -21,26 +22,25 @@ def write_csv(results, output_path):
                 "license_number_score",
             ]
         )
-        for frame_nmr, data in results.items():
-            for car_id, info in data.items():
-                if (
-                    "vehicle" in info
-                    and isinstance(info["vehicle"], Vehicle)
-                    and info["vehicle"].plate is not None
-                ):
-                    vehicle = info["vehicle"]
-                    plate = vehicle.plate
-                    # Usa as 4 primeiras coordenadas do bbox do veículo
-                    car_bbox = vehicle.bbox[:4]
-                    license_plate_bbox = plate.bbox
-                    writer.writerow(
-                        [
-                            frame_nmr,
-                            car_id,
-                            f"[{' '.join(str(x) for x in car_bbox)}]",
-                            f"[{' '.join(str(x) for x in license_plate_bbox)}]",
-                            plate.detection_confidence,
-                            plate.text,
-                            plate.ocr_confidence,
-                        ]
-                    )
+        for track_id, data in results.items():
+            if (
+                "vehicle" in data
+                and isinstance(data["vehicle"], Vehicle)
+                and data["vehicle"].plate is not None
+            ):
+                vehicle = data["vehicle"]
+                plate = vehicle.plate
+                # Usa as 4 primeiras coordenadas do bbox do veículo
+                car_bbox = vehicle.bbox[:4]
+                license_plate_bbox = plate.bbox
+                writer.writerow(
+                    [
+                        vehicle.frame_nmr,
+                        track_id,
+                        f"[{' '.join(str(x) for x in car_bbox)}]",
+                        f"[{' '.join(str(x) for x in license_plate_bbox)}]",
+                        plate.detection_confidence,
+                        plate.text,
+                        plate.ocr_confidence,
+                    ]
+                )
