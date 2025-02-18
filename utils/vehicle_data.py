f593813b680a7478667aa06cebab8f9b979ab562
Caio Wingeter <castilhocaio1164@gmail.com>
2025-02-17 21:21:25 -0300

    Add docstring to Vehicle class and introduce frame_nmr attribute for better clarity

--- utils/vehicle_data.py
+++ utils/vehicle_data.py
@@ -5,7 +5,9 @@ from utils.plate_data import LicensePlate
 
 @dataclass
 class Vehicle:
+    "Estrutura para representar um veículo"
     bbox: List[float]  # [x1, y1, x2, y2, score]
     id: int
     label: str = "car"
     plate: Optional[LicensePlate] = None
+    frame_nmr: int = 0  # Número do frame da detecção final
