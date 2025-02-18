2e03a056499485ff956724abf51698d2cfc1135e
Caio Wingeter <castilhocaio1164@gmail.com>
2025-02-17 21:21:22 -0300

    Refactor image preprocessing to directly convert to grayscale for improved clarity

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -3,6 +3,6 @@ import cv2
 
 def preprocess_license_plate(image):
     """Melhora o contraste para OCR."""
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # _, thresh = cv2.threshold(gray, 64, 255, cv2.THRESH_BINARY_INV)
     return image
