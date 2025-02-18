de0910061d2f245c28bde5841940d2cbb2dff4bb
Caio Wingeter <castilhocaio1164@gmail.com>
2025-02-17 21:21:18 -0300

    Add docstring to LicensePlate class for better clarity

--- utils/plate_data.py
+++ utils/plate_data.py
@@ -4,6 +4,7 @@ from typing import List
 
 @dataclass
 class LicensePlate:
+    "Estrutura para representar uma placa"
     bbox: List[float]  # [x1, y1, x2, y2]
     text: str
     detection_confidence: float = 0.0  # Score da detecção da placa (SAHI)
